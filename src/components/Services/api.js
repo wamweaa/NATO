10c7bae5eb5af1d6c561d66c367420c96471c102
wamweaa <patokinya12@gmail.com>
2025-01-21 11:36:56 +0300

    fixing authentication error

--- src/components/Services/api.js
+++ src/components/Services/api.js
@@ -1,12 +1,26 @@
 import axios from 'axios'
-
+import { getToken } from './auth';
 const API = axios.create({
   baseURL: 'http://127.0.0.1:5000/api'
 });
+
+// interceptor
+API.interceptors.request.use(
+  (config) => {
+    const token = getToken();
+    if (token){
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
 
 export const login = (data) => API.post('/login', data);
 export const register = (data) => API.post('/register', data);
 export const getRecords = () => API.get('/records');
 export const addRecords = (data) => API.post('/records', data);
-export const deleteRecords = (id) => API.delete('/records/${id}'); 
+export const deleteRecords = (id) => API.delete(`/records/${id}`); 
 export const getAllUsers = () => API.get('/admin/users');
